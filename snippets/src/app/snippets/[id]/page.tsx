079c271a43d308610bacf61eafadfc78dde47f81
MaorHadadLD <maorhadad94@gmail.com>
2024-12-04 21:21:24 +0200

    updating the dynamic paths

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import {notFound} from 'next/navigation';
 import {db} from '@/db';
 
 interface SnippetShowPageProps {
@@ -8,5 +9,12 @@ interface SnippetShowPageProps {
 
 
 export default async function SnippetShowPage(props: SnippetShowPageProps){
-    return <div>Show a Snippet</div>
+    const snippet = await db.snippet.findFirst({
+        where: {id: parseInt(props.params.id)}
+    });
+
+    if(!snippet) {
+        return notFound();
+    }
+    return <div>{snippet.title}</div>
 }
