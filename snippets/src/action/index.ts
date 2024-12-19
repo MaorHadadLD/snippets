b1d46d0c36c8681a0de8e6a47e22efab7add9841
MaorHadadLD <maorhadad94@gmail.com>
2024-12-19 17:12:55 +0200

    add error message

--- snippets/src/action/index.ts
+++ snippets/src/action/index.ts
@@ -19,3 +19,46 @@ export async function deleteSnippet(id: number) {
 
     redirect('/');  
 }
+
+export async function createSnippet(formState: {message: string}, FormData: FormData){
+    try{
+            // Check the user`s input and make sure they're valid
+            const title = FormData.get('title');
+             const code = FormData.get('code') ;
+
+                if (typeof title !== 'string' || title.length < 3) {
+                    return {
+                        message: 'title must be longer',
+                    };
+                }
+                if (typeof code !== 'string' || code.length < 10) {
+                        return {
+                                message: 'Code must be longer',
+                            };
+                }  
+
+
+            // Create a new record in the database
+                // const snippet = await db.snippet.create({
+                //     data: {
+                //      title,
+                //       code
+                //     },
+                // });
+                //     console.log(snippet);
+                throw new Error('Faild to save to database.');
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    return {
+                        message: err.message
+                    };
+                }else {
+                    return {
+                        message: 'Something went wrong...'
+                    }
+                }
+            }
+
+    // Redirect the user back to the root route
+    redirect('/');
+}
